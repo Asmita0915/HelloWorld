0726bd4b95fca7bcff0de7ba2441bc142fa341db
Asmita0915 <asmitamutgekar@gmail.com>
2017-10-25 12:12:12 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 class HelloWorld{
   public static void main(String args[]){
     System.out.println("HelloWorld");
+    message("ABC");
+  }
+  public static message(String string){
+    System.out.println("Welcome "+string);
   }
 }
